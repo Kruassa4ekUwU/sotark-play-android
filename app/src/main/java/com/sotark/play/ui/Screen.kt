d64083f29e3475361a36382e6cb370f44eccd4db
Kruassa4ekUwU <kruassa4ek@gmail.com>
2026-06-05 22:19:58 +0300

    feat: add Publish screen route

--- app/src/main/java/com/sotark/play/ui/Screen.kt
+++ app/src/main/java/com/sotark/play/ui/Screen.kt
@@ -1,10 +1,10 @@
 package com.sotark.play.ui
 
 sealed class Screen(val route: String) {
-    object Home   : Screen("home")
-    object Search : Screen("search")
-    object Categories : Screen("categories")
-    object AppDetail  : Screen("app/{appId}") {
+    object Home      : Screen("home")
+    object Search    : Screen("search")
+    object Publish   : Screen("publish")
+    object AppDetail : Screen("app/{appId}") {
         fun createRoute(id: Int) = "app/$id"
     }
 }
